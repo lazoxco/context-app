873762e1af7338a1a00ba9927ba9effbcfbd2505
Nory Lazo <nlazo@me.com>
2021-10-05 23:21:54 -0400

    Added auth context.

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import React from 'react';
 import Navbar from './components/Navbar'
 import BookList from './components/BookList'
 import ThemeContextProvider from './contexts/ThemeContext'
+import AuthContextProvider from './contexts/AuthContext'
 import ThemeToggle from './components/ThemeToggle'
 
 function App() {
   return (
     <div className="App">
       <ThemeContextProvider>
-        <Navbar />
-        <BookList />
-        <ThemeToggle />
+        <AuthContextProvider>
+          <Navbar />
+          <BookList />
+          <ThemeToggle />
+        </AuthContextProvider>
       </ThemeContextProvider>
     </div>
   );
